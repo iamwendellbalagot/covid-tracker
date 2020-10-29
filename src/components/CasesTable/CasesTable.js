b7d36ddc186b3680f71fdd894f2b38a9b1acf60f
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-10-30 03:20:42 +0800

    Finished the TABLE

--- src/components/CasesTable/CasesTable.js
+++ src/components/CasesTable/CasesTable.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CasesTable.css';
 
 function CasesTable({countries}) {
+    const [sortedData, setSortedData] = useState(countries);
+    useEffect(() =>{
+        setSortedData(countries.sort((a,b) => a.cases > b.cases? -1 : 1))
+    }, [countries])
+
     return (
         <div className='table'>
             <h3>Live Cases by Country</h3>
             <div className='table__cases'>
-                {countries?.map(country =>(
+                {sortedData?.map(country =>(
                     <tr key ={country.country}>
                         <td>{country.country}</td>
                         <strong><td>{country.cases}</td></strong>
